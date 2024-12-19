9c67207e4fdd3a9fb3f8582da3f2f92ce7c86cfe
jdsull3002 <jeremy.sullivan@zimmerbiomet.com>
2024-12-18 17:45:13 -0800

    Updates

--- assets/keyboard-2cf6e2a4.js
+++ assets/keyboard-2cf6e2a4.js
@@ -1,4 +1,4 @@
-import{w as t}from"./index-ed28c7fe.js";/*!
+import{w as t}from"./index-eb62bafc.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const r=()=>{if(t!==void 0)return t.Capacitor};/*!
  * (C) Ionic http://ionicframework.com - MIT License
--- assets/status-tap-a65f5633.js
+++ assets/status-tap-a65f5633.js
@@ -1,3 +1,3 @@
-import{a,b as i,e as c}from"./index-ed28c7fe.js";import{f as d,s as l}from"./index8-6cf749a8.js";/*!
+import{a,b as i,e as c}from"./index-eb62bafc.js";import{f as d,s as l}from"./index8-65f7d794.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
